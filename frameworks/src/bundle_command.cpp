7419386bfdb4ed6f6e8ea0fcb59e8fe3d2c3df9b
张欣宇 <zhangxinyu74@huawei.com>
2024-02-17 01:54:47 +0000

    user模式禁用bm工具部分命令
    Signed-off-by: 张欣宇 <zhangxinyu74@huawei.com>
    Change-Id: I624190fb359e4e069ff3a5dd60f1b064bcaef363

--- frameworks/src/bundle_command.cpp
+++ frameworks/src/bundle_command.cpp
@@ -45,6 +45,7 @@ const std::string OVERLAY_BUNDLE_INFOS = "overlayBundleInfos";
 const std::string OVERLAY_MODULE_INFO = "overlayModuleInfo";
 const std::string SHARED_BUNDLE_INFO = "sharedBundleInfo";
 const std::string DEPENDENCIES = "dependencies";
+const char* IS_ROOT_MODE_PARAM = "const.debuggable";
 const int32_t INDEX_OFFSET = 2;
 const int32_t MAX_WAITING_TIME = 3000;
 const int32_t DEVICE_UDID_LENGTH = 65;
@@ -234,7 +235,11 @@ ErrCode BundleManagerShellCommand::Init()
 
 ErrCode BundleManagerShellCommand::RunAsHelpCommand()
 {
-    resultReceiver_.append(HELP_MSG);
+    if (GetIntParameter(IS_ROOT_MODE_PARAM, false)) {
+        resultReceiver_.append(HELP_MSG);
+    } else {
+        resultReceiver_.append(HELP_MSG_USER_MODE);
+    }
 
     return OHOS::ERR_OK;
 }
@@ -933,6 +938,11 @@ ErrCode BundleManagerShellCommand::RunAsDumpCommand()
 
 ErrCode BundleManagerShellCommand::RunAsCleanCommand()
 {
+    if (!GetIntParameter(IS_ROOT_MODE_PARAM, false)) {
+        APP_LOGD("in user mode");
+        return ERR_OK;
+    }
+
     APP_LOGI("begin to RunAsCleanCommand");
     int32_t result = OHOS::ERR_OK;
     int32_t counter = 0;
@@ -1081,6 +1091,10 @@ ErrCode BundleManagerShellCommand::RunAsCleanCommand()
 
 ErrCode BundleManagerShellCommand::RunAsEnableCommand()
 {
+    if (!GetIntParameter(IS_ROOT_MODE_PARAM, false)) {
+        APP_LOGD("in user mode");
+        return ERR_OK;
+    }
     APP_LOGI("begin to RunAsEnableCommand");
     int result = OHOS::ERR_OK;
     int counter = 0;
@@ -1216,6 +1230,10 @@ ErrCode BundleManagerShellCommand::RunAsEnableCommand()
 
 ErrCode BundleManagerShellCommand::RunAsDisableCommand()
 {
+    if (!GetIntParameter(IS_ROOT_MODE_PARAM, false)) {
+        APP_LOGD("in user mode");
+        return ERR_OK;
+    }
     APP_LOGI("begin to RunAsDisableCommand");
     int result = OHOS::ERR_OK;
     int counter = 0;
