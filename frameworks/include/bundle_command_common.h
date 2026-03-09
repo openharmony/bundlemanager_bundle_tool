af8b14b4ed5efedd80f9a822ab4d8c1d5f517272
lanhaoyu <lanhaoyu3@huawei-partners.com>
2026-03-09 09:26:43 +0800

    dbms on demand start stop
    Signed-off-by: lanhaoyu <lanhaoyu3@huawei-partners.com>

--- frameworks/include/bundle_command_common.h
+++ frameworks/include/bundle_command_common.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,9 +18,6 @@
 
 #include "bundle_mgr_interface.h"
 #include "bundle_installer_interface.h"
-#ifdef DISTRIBUTED_BUNDLE_FRAMEWORK
-#include "distributed_bms_interface.h"
-#endif
 
 #include <map>
 #include <string>
@@ -30,10 +27,6 @@ namespace AppExecFwk {
 class BundleCommandCommon {
 public:
     static sptr<IBundleMgr> GetBundleMgrProxy();
-
-#ifdef DISTRIBUTED_BUNDLE_FRAMEWORK
-    static sptr<IDistributedBms> GetDistributedBundleMgrService();
-#endif
 
     static int32_t GetCurrentUserId(int32_t userId);
 
